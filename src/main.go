30d87549b34c8dda9aa58fde5c6b54cbce469ddd
Kroese <kroese@users.noreply.github.com>
2026-08-01 08:36:03 +0200

    Refactor error output to use stderr variable

--- src/main.go
+++ src/main.go
@@ -2,14 +2,19 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 )
 
-var Version = "0.0"
+var (
+	Version = "0.0"
+	stdout  io.Writer = os.Stdout
+	stderr  io.Writer = os.Stderr
+)
 
 func main() {
 	if err := run(os.Args[1:]); err != nil {
-		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		fmt.Fprintf(stderr, "Error: %v\n", err)
 		os.Exit(1)
 	}
 }
